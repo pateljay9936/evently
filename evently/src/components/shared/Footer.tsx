fc06496eef49241c4c08693f5234ba5f52b03e67
jay_patel <pateljay9936@gmail.com>
2024-02-10 01:33:43 -0500

    footer added

--- evently/src/components/shared/Footer.tsx
+++ evently/src/components/shared/Footer.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer(){
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex flex-col flex-center flex-between wrapper p-5 gap-4 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image 
+              src='assets/images/logo.svg' 
+              alt='Evently' 
+              width={128} 
+              height={38}
+          />
+        </Link>
+        <p>2024 Evently. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
