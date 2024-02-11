a419505ab95ca631ec23dfdcf6f9f6360a3e7b66
jay_patel <pateljay9936@gmail.com>
2024-02-11 00:22:30 -0500

    added routes for user creation,updation and deletion with use of  webhooks AND added a file for mongoDB connection function

--- evently/src/app/(root)/page.tsx
+++ evently/src/app/(root)/page.tsx
@@ -1,13 +1,39 @@
+import Link from "next/link";
 import {Button} from "../../components/ui/button";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    <div>
-      {/* <h1 className="text-4xl">Evently</h1>
-      <Button variant="destructive" className="px-4 ">
-        hello
-      </Button> */}
-    </div>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0 ">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Event, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <Button asChild className="w-full sm:w-fit button" size='lg'>
+              <Link href='#events'>Explore Now</Link>
+            </Button>
+          </div>
+          <Image
+          className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+          />
+
+        </div>
+      </section>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br/>Thousand of Events </h2>   
+
+        <div className="flex flex-col w-full md:flex-row gap-5">
+            search_bar
+            catogory_filter
+        </div>
+
+      </section>
+    </>
   );
 }
